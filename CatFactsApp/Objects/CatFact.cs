596f5f33da2aab0b5a88149d9879f00363b3d4b9
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2025-07-17 15:24:18 +0200

    Initial commit

---
+++ CatFactsApp/Objects/CatFact.cs
@@ -0,0 +1,8 @@
+﻿namespace CatFactsApp.Objects
+{
+    internal class CatFact
+    {
+        public required string Fact { get; set; }
+        public required int Length { get; set; }
+    }
+}
---
+++ CatFactsApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CatFactsApp
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var endpoint = new Uri("https://catfact.ninja/fact");
+                    var result = client.GetAsync(endpoint).Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var json = await result.Content.ReadAsStringAsync();
+                        Console.WriteLine(json);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {result.StatusCode}");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+        }
+    }
+}
